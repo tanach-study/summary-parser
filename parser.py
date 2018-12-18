d0fcc723b10d4b6f81f70fafa5d6dbfb89ca6f9e
Joey Pinhas <jpinhas97@yahoo.com>
2018-12-18 14:27:13 -0500

    Clean up comments and add abstraction over book names

--- parser.py
+++ parser.py
@@ -26,7 +26,45 @@ replacements = {
     8220: '"',
     8221: '"',
     8226: '-',
+    8230: '...',
     8232: ' ',
+}
+
+book_name_mappings = {
+    'yehoshua': 'yehoshua',
+    'shofetim': 'shofetim',
+    'shemuel1': 'shemuel1',
+    'shemuel2': 'shemuel2',
+    'melachim1': 'melachim1',
+    'melachim2': 'melachim2',
+    'yeshayahu': 'yeshayahu',
+    'yirmiyahu': 'yirmiyahu',
+    'yehezkel': 'yehezkel',
+    'hoshea': 'hoshea',
+    'yoel': 'yoel',
+    'amos': 'amos',
+    'ovadia': 'ovadiah',
+    'yonah': 'yonah',
+    'michah': 'michah',
+    'nahum': 'nahum',
+    'habakuk': 'habakuk',
+    'sephania': 'sefaniah',
+    'hagai': 'hagai',
+    'zecharia': 'zecharia',
+    'malachi': 'malachi',
+    'divre-hayamim-1': 'divre-hayamim-1',
+    'divre-hayamim-2': 'divre-hayamim-2',
+    'tehillim': 'tehillim',
+    'mishle': 'mishle',
+    'iyov': 'iyov',
+    'shir-hashirim': 'shir-hashirim',
+    'ruth': 'ruth',
+    'eichah': 'eichah',
+    'kohelet': 'kohelet',
+    'esther': 'esther',
+    'daniel': 'daniel',
+    'ezra': 'ezra',
+    'nehemya': 'nehemya',
 }
 
 # get a csv output file
@@ -40,10 +78,8 @@ with open("summaries.csv", mode="w", encoding="ascii") as out_file:
     for book in data:
         if book["seferMeta"]["part_id"] != 5:
             # get the name of the current sefer
-            sefer = book["seferMeta"]["book_name_pretty_eng"]
-            match = re.match(r'(\w+)(\d+)', sefer)
-            if match:
-                sefer = str.join('-', match.groups());
+            sefer = book["seferMeta"]["book_name"]
+            sefer = book_name_mappings[sefer]
             # loop through all the perakim in the sefer
             for perek in book["allPerakim"]:
                 if perek["perek_id"] == 0:
@@ -51,15 +87,15 @@ with open("summaries.csv", mode="w", encoding="ascii") as out_file:
                 else:
                     perek_num = str(perek["perek_id"])
 
-                file_name = "./site/" + sefer.lower() + "-" + perek_num + ".html"
-                print("attempting to open", file_name)
+                file_name = "./site/" + sefer + "-" + perek_num + ".html"
+                # print("attempting to open", file_name)
                 if os.path.isfile(file_name.encode('ascii')):
                     with open(file_name.encode('ascii'), mode='r') as perek_file:
                         soup = BeautifulSoup(perek_file, 'html.parser')
                         summaries = soup.find_all(class_="perek-summary")
                         final = ""
                         for summary in summaries:
-                            print(summary)
+                            # print(summary)
                             str_summary = str(summary)
                             # str_summary.replace(chr(8211), '-')
                             # str_summary.replace(chr(8216), '\'')
@@ -76,23 +112,25 @@ with open("summaries.csv", mode="w", encoding="ascii") as out_file:
                             # str_summary.replace(u"\u2022", '-')
                             # str_summary.replace(u"\u2028", '\n')
                             clean = re.sub(pattern, ' ', str_summary)
-                            new_soup = BeautifulSoup(clean, 'html.parser', from_encoding='utf-8')
+                            new_soup = BeautifulSoup(clean, 'html.parser')
                             temp = new_soup.text
                             temp_list = list(temp)
                             for i, c in enumerate(temp_list):
                                 if ord(c) > 127:
-                                    print(c)
+                                    # print(c)
                                     non_ascii_characters.append(c)
                                     temp_list[i] = replacements[ord(c)]
                             temp = "".join(temp_list)
                             if len(temp) > len(final):
                                 final = temp
                             # print(new_soup.original_encoding)
-                            print()
-                        print()
-                        print()
+                        #     print()
+                        # print()
+                        # print()
                         final.encode('ascii')
                         out_writer.writerow([sefer, perek_num, final])
+                else:
+                    print(file_name, "DNE")
 
 with open("non_ascii.txt", mode="w", encoding="utf-8") as out_file:
     non_ascii_character_set = set(non_ascii_characters)
